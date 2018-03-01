eca47585f7512d6963ab4b751233b3583039cee7
hzhang1902 <zhanghaofan999@gmail.com>
2018-03-01 15:57:56 -0500

    database url env

--- firebase.js
+++ firebase.js
@@ -2,11 +2,15 @@ var admin = require("firebase-admin");
 
 
 function getServiceAccount() {
-	return JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+	if(process.env.NODE_ENV == "production")
+		return JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+	else 
+		return require('./carliewpi-ac614-firebase-adminsdk-qhsry-1289cb185e.json')
 }
 
+
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
+	credential: admin.credential.cert(getServiceAccount()),
 	databaseURL: process.env.FIREBASE_DATABASEURL
 });
 
