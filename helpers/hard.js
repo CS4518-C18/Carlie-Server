e91f14ca950b3330c46b3c5fb6b02802052623b5
hzhang1902 <zhanghaofan999@gmail.com>
2018-03-01 16:09:26 -0500

    trying

--- helpers/hard.js
+++ helpers/hard.js
@@ -1,3 +1,6 @@
 var ref = require('../firebase')
+var json = require('../carliewpi-ac614-firebase-adminsdk-qhsry-1289cb185e.json')
 
-ref.child('shuttles').child('shuttle1').child('trips').child('www').set(0)
+//ref.child('shuttles').child('shuttle1').child('trips').child('www').set(0)
+
+console.log(JSON.stringify(json))
